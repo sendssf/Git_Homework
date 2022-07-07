5276955d643643ae4e424714c23d586080bf19b0
sky <2336526392@qq.com>
2022-07-07 15:25:35 +0800

    another one

--- hello.cpp
+++ hello.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
+using  namespace std;
 
 int main(int argc, char **argv){
     std::cout << "Hello, world!" << std::endl;
+    cout << "Usage: " << argv[0] << std::endl;
     return 0;
 }
