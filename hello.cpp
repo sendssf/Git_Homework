4fd7720714446acfdff2b21dce5289bf51f7a50a
sky <2336526392@qq.com>
2022-07-07 17:38:20 +0800

    delete others

--- hello.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using  namespace std;
-
-int main(int argc, char **argv){
-    std::cout << "Hello, world!" << std::endl;
-    cout << "Usage: " << argv[0] << std::endl;
-    return 0;
-}
